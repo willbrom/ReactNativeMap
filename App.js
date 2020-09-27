4fb0e35e59ebe8feb4da2bf1d22ab50083eec710
Ali Sajid <alisajid552@yahoo.com>
2020-09-27 13:00:35 -0800

    add marker keys

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
 
 import { Searchbar, Banner, Button } from "react-native-paper";
 import MapView, { Marker } from "react-native-maps";
-import { BottomSheet } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
@@ -21,51 +20,61 @@ export default function App() {
 
   const markers = [
     {
+      key: 1,
       title: "2003",
       description: "status: up",
       latlng: { latitude: 21.420087, longitude: 39.828325 },
     },
     {
+      key: 2,
       title: "1923",
       description: "status: down",
       latlng: { latitude: 21.429042, longitude: 39.832721 },
     },
     {
+      key: 3,
       title: "3123",
       description: "status: down",
       latlng: { latitude: 21.42631, longitude: 39.819262 },
     },
     {
+      key: 4,
       title: "4213",
       description: "status: up",
       latlng: { latitude: 21.422033, longitude: 39.826677 },
     },
     {
+      key: 5,
       title: "2132",
       description: "status: up",
       latlng: { latitude: 21.385234, longitude: 39.764184 },
     },
     {
+      key: 6,
       title: "2311",
       description: "status: down",
       latlng: { latitude: 21.350818, longitude: 39.803525 },
     },
     {
+      key: 7,
       title: "6131",
       description: "status: up",
       latlng: { latitude: 21.359551, longitude: 39.87371 },
     },
     {
+      key: 8,
       title: "2134",
       description: "status: up",
       latlng: { latitude: 21.455801, longitude: 39.807876 },
     },
     {
+      key: 9,
       title: "5231",
       description: "status: up",
       latlng: { latitude: 21.454688, longitude: 39.812666 },
     },
     {
+      key: 10,
       title: "3213",
       description: "status: up",
       latlng: { latitude: 21.440646, longitude: 39.839242 },
@@ -107,6 +116,7 @@ export default function App() {
         {markers.map((marker) => {
           return (
             <Marker
+              key={marker.key}
               title={marker.title}
               description={marker.description}
               coordinate={marker.latlng}
